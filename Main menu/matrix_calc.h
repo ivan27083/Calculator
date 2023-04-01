ceba7dd301c2c9bc2709d468be1350e7295a38fa
Ann-klek <75799146+Ann-klek@users.noreply.github.com>
2023-04-01 06:12:27 +0300

    Добавила функции в заголовочный файл

--- Main menu/matrix_calc.h
+++ Main menu/matrix_calc.h
@@ -3,16 +3,22 @@
 //  matrica.h
 //  matrica
 //
-//  Created by Aíía Êëåêîâêèía on 18.03.2023.
+//  Created by AÃ­Ã­a ÃŠÃ«Ã¥ÃªÃ®Ã¢ÃªÃ¨Ã­a on 18.03.2023.
 //
 #ifndef matrica_h
 #define matrica_h
 #include <iostream>
 #include <malloc.h>
+#include <math.h>
 void menu_m();
 void umnm();
 void transp();
 void umnch();
 void minusm();
 void plusm();
+void opredm();
+void plus_minus_ch();
+void inversem();
+struct Minor;
+double determm(Minor minor, int n);
 #endif /* matrica_h */
